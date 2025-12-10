a20435229bf48e03eaab25c5156eb50c1b7b3869
Spokie482 <pachamefabrizio18@gmail.com>
2025-12-10 08:52:33 -0300

    superusuario temporal

--- build.sh
+++ build.sh
@@ -10,3 +10,6 @@ python manage.py collectstatic --noinput
 
 # Aplicar migraciones a la base de datos de producción
 python manage.py migrate
+
+# Crear superusuario automáticamente si no existe
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin_joyas').exists() or User.objects.create_superuser('marianlea', 'admin@ejemplo.com', 'joyas123')"
